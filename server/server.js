d0ad6f839a990ccd76ae97e71d6db4eb6e93d3d0
Amaan <142074588+Amaan-Khan14@users.noreply.github.com>
2025-02-10 18:27:50 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/path', (req, res) => {
-    res.send('Hello from path')
-})
+app.get('/path', (req, res) => { res.send('Hello from path') })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
